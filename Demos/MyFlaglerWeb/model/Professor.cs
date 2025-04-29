0260e18dab6d7fb228484d7ab66c175440abac07
Jeremy Wang <JWang@flagler.edu>
2025-04-29 17:03:29 -0400

    4-29-2025

--- Demos/MyFlaglerWeb/model/Professor.cs
+++ Demos/MyFlaglerWeb/model/Professor.cs
@@ -4,7 +4,8 @@ public class Professor : Person
 {
     public string Department { get; set; }
     public string ResearchArea { get; set; }
-    public bool IsTerminalDegree { get; set; } 
+    public bool IsTerminalDegree { get; set; }
+
     public override string GetDetails()
     {
         return $"{Name}: {Department} Department | Research: {ResearchArea} | Terminal Degree: {IsTerminalDegree}";
